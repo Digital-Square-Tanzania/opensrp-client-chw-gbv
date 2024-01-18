6a4c24d4270514da5a60e5e0adf885db6b5c4438
cozej4 <ilakozejumanne@gmail.com>
2024-01-18 16:58:40 +0300

    :hammer: Refactored BaseGbvRegisterModel getFormAsJson

--- opensrp-chw-gbv/src/main/java/org/smartregister/chw/gbv/model/BaseGbvRegisterModel.java
+++ opensrp-chw-gbv/src/main/java/org/smartregister/chw/gbv/model/BaseGbvRegisterModel.java
@@ -2,6 +2,8 @@ package org.smartregister.chw.gbv.model;
 
 import org.json.JSONObject;
 import org.smartregister.chw.gbv.contract.GbvRegisterContract;
+import org.smartregister.chw.gbv.dao.GbvDao;
+import org.smartregister.chw.gbv.domain.MemberObject;
 import org.smartregister.chw.gbv.util.GbvJsonFormUtils;
 
 public class BaseGbvRegisterModel implements GbvRegisterContract.Model {
@@ -9,6 +11,14 @@ public class BaseGbvRegisterModel implements GbvRegisterContract.Model {
     @Override
     public JSONObject getFormAsJson(String formName, String entityId, String currentLocationId) throws Exception {
         JSONObject jsonObject = GbvJsonFormUtils.getFormAsJson(formName);
+
+
+        if (jsonObject.has("global")) {
+            MemberObject memberObject = GbvDao.getMember(entityId);
+            jsonObject.getJSONObject("global").put("age", memberObject.getAge());
+            jsonObject.getJSONObject("global").put("gender", memberObject.getGender());
+        }
+
         GbvJsonFormUtils.getRegistrationForm(jsonObject, entityId, currentLocationId);
 
         return jsonObject;
